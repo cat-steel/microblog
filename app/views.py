b7b7133a44c039617997805ee18be7717d4854ac
cat-steel <497051142@qq.com>
2018-07-30 22:18:49 +0800

    编辑按钮与样式调整

--- app/views.py
+++ app/views.py
@@ -19,32 +19,67 @@ def add_testcase():
             server = request.form['server']
             ways = request.form['way']
             request_method = request.form['request_method']
+            print('请求方式：%s' % request_method)
             data_i = request.form['data_i']
             data = request.form['data']
             check = request.form['check']
-            is_do = request.form['is_do']
             is_base = request.form['is_base']
             is_base_do = request.form['is_base_do']
-            category = Case(name,server,ways,request_method,data_i,data,check,is_do,is_base,is_base_do)
+            category = Case(name,server,ways,request_method,data_i,data,check,'',is_base,is_base_do)
             db.session.add(category)
             db.session.commit()
             flash('用例保存成功')
             return redirect(url_for('show'))
 
-@app.route('/delete')
+@app.route('/delete/', methods=['GET', 'POST'])
 def delete():
-    Case.query.filter_by(id=ids).delete()
-    #print('ids:%s'%ids)
-    #db.session.delete(ids)
-    db.session.commit()
-    return redirect(url_for('show'))
+    if request.method =='GET':
+        if not session.get('logged_in'):
+            return redirect('login')
+        else:
+            ids = request.args.get('id')
+            id = int(ids)
+            Case.query.filter_by(id=id).delete()
+            db.session.commit()
+            flash('删除完成')
+            return redirect(url_for('show'))
+
+@app.route('/editor/', methods=['GET', 'POST'])
+def editor():
+    if request.method == 'POST':
+        if not session.get('logged_in'):
+            return redirect('login')
+        else:
+            id_num = request.form['num1']
+            name = request.form['names1']
+            server = request.form['server1']
+            ways = request.form['way1']
+            request_method = request.form['request_method1']
+            data_i = request.form['data_i1']
+            data = request.form['data1']
+            check = request.form['check1']
+            is_base = request.form['is_base1']
+            is_base_do = request.form['is_base_do1']
+            Case.query.filter_by(id=id_num).update({Case.name:name,
+                                              Case.IP:server,
+                                              Case.ways:ways,
+                                              Case.request_method:request_method,
+                                              Case.data_i:data_i,
+                                              Case.data:data,
+                                              Case.check:check,
+                                              Case.is_do:'',
+                                              Case.is_base:is_base,
+                                              Case.is_base_do:is_base_do})
+            db.session.commit()
+            flash('用例更新成功')
+            return redirect(url_for('show'))
+
 @app.route('/')
 def show():
     if not session.get('logged_in'):
         return redirect('login')
     else:
         cases = Case.query.all()
-        print('categorys:%s'%cases)
         return render_template("testcase.html",
                                title='Home',
                                cases=cases)
@@ -65,7 +100,6 @@ def login():
         else:
             session['logged_in'] = True
             flash('You were logged in')
-            print('开始重定向转到主页')
             return redirect(url_for('show'))
     return render_template('login.html',
                            title='登录',
